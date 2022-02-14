a1e7a4147467dbe4df0d47fab3634a102ed8643e
Kngz91 <koval_alexander@hotmail.com>
2022-02-14 12:13:04 +0300

    Fix 1 14.02

---
+++ src/com/company/letterChoice.java
@@ -0,0 +1,15 @@
+//package com.company;
+//
+//public enum letterChoice {
+//    Y ("y"),N ("n");
+//
+//    private static final String value;
+//
+//    letterChoice (String value){
+//        this.value = value;
+//    }
+//
+//    public static String getValue(){
+//        return value;
+//    }
+//}
